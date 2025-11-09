554734a210299f4aa3f48a4884bdcdb71265c8b9
MigueltrejoEsp <mangeel008@gmail.com>
2025-11-09 09:28:17 -0300

    feat: Username validation

--- lib/live_chat_web/live/home_live.ex
+++ lib/live_chat_web/live/home_live.ex
@@ -4,28 +4,46 @@ defmodule LiveChatWeb.HomeLive do
 
   def mount(%{"chat_id" => chat_id}, _session, socket) do
     PubSub.subscribe(LiveChat.PubSub, chat_id)
-    {:ok, assign(socket, chat_id: chat_id, form: to_form(%{}), messages: [])}
+    {:ok, assign(socket, chat_id: chat_id, form: to_form(%{}), messages: [], username: nil)}
   end
 
   def render(assigns) do
     ~H"""
-    <div class="flex flex-col">
-      <%= for {pid, message} <- @messages do %>
-        <div class={"border p-2 bg-gray-300 #{if pid == self(), do: "self-start text-start", else: "self-end text-end"}"}>
-          <p>{inspect(pid)}:</p>
-          <p>{message}</p>
+    <%= if !@username do %>
+      <div>
+        <div>
+          <h2>Welcome to Live Chat!</h2>
+          <p>Tell us your username.</p>
+          <.form for={@form} id="user-name" phx-submit="set_username">
+            <.input type="text" field={@form[:username]} />
+            <button>Start</button>
+          </.form>
         </div>
-      <% end %>
-    </div>
-    <.form for={@form} id="chat-form" phx-submit="send_message">
-      <.input type="text" field={@form[:message]} />
-      <button>Save</button>
-    </.form>
+      </div>
+    <% end %>
+    <%= if @username do %>
+      <div class="flex flex-col">
+        <%= for {username, message} <- @messages do %>
+          <div class={"border p-2 bg-gray-300 #{if username == @username, do: "self-end text-end", else: "self-start text-start"}"}>
+            <p>{inspect(username)}:</p>
+            <p>{message}</p>
+          </div>
+        <% end %>
+      </div>
+      <.form for={@form} id="chat-form" phx-submit="send_message">
+        <.input type="text" field={@form[:message]} />
+        <button>Save</button>
+      </.form>
+    <% end %>
     """
   end
 
+  def handle_event("set_username", %{"username" => username}, socket) do
+    {:noreply, assign(socket, username: username, form: to_form(%{}))}
+  end
+
   def handle_event("send_message", %{"message" => message}, socket) do
-    PubSub.broadcast(LiveChat.PubSub, socket.assigns.chat_id, {self(), message})
+    PubSub.broadcast(LiveChat.PubSub, socket.assigns.chat_id, {socket.assigns.username, message})
     {:noreply, socket}
   end
 

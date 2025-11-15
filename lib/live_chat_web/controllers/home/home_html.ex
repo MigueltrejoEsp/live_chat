00d8a48001020363d0f66c703bab25474c846420
MigueltrejoEsp <mangeel008@gmail.com>
2025-11-14 21:16:10 -0300

    feat: Chatroom form

--- lib/live_chat_web/controllers/home/home_html.ex
+++ lib/live_chat_web/controllers/home/home_html.ex
@@ -12,6 +12,11 @@ defmodule LiveChatWeb.HomeHTML do
         <p class="text-lg">
           This is a simple live chat application built with Phoenix.
         </p>
+
+        <.form :let={_f} for={%{}} action={~p"/redirect-to-chat"} method="post">
+          <input type="text" name="chat_id" placeholder="Enter chat room..." required />
+          <button type="submit">Go to Chat</button>
+        </.form>
       </div>
     </Layouts.app>
     """
